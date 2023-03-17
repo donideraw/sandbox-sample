a798f073eb404f1b1d2c4410fb11418c154ddd80
donideraw <dwibisana@alamisharia.co.id>
2023-03-17 11:00:04 +0700

    init springboot project

---
+++ src/test/java/com/doni/sandboxbackend/SandboxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doni.sandboxbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandboxBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
