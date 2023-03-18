3004e8cb6f3522a3d0aae940a8076161eee3a718
donideraw <dwibisana@alamisharia.co.id>
2023-03-18 16:53:22 +0700

    Trigger jenkins

--- src/main/java/com/doni/sandboxbackend/controller/HealthCheckController.java
+++ src/main/java/com/doni/sandboxbackend/controller/HealthCheckController.java
@@ -9,7 +9,7 @@ public class HealthCheckController {
 
     @GetMapping("/health")
     public ResponseEntity<String> checkHealth() {
-        return ResponseEntity.ok("Application is running..! ");
+        return ResponseEntity.ok("Application is running...! ");
     }
 
 }
